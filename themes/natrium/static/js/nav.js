059a7e66ea4f33a8a4feedb101970c9ef6a24b46
mudox <imudox@gmail.com>
2018-07-29 04:51:54 +0800

    Improve pane.js

--- themes/natrium/static/js/nav.js
+++ themes/natrium/static/js/nav.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
   $(window).scroll(function() {
     showPaneNavIfNotTop()
-    scrollPane()
+    scrollPanes()
   })
 });
 
 function showPaneNavIfNotTop() {
-  if (window.pageYOffset > 10) {
+  // 70 here ~= height of header
+  if (window.pageYOffset > 70) {
     $('.pane-nav').fadeIn()
   } else {
     $('.pane-nav').fadeOut()
   }
 }
 
-function scrollPane() {
+function scrollPanes() {
   var topStart = 128; // should be the same as in `pane.css`
   var topEnd = 20;
   var topRange = topStart - topEnd;
@@ -24,13 +25,17 @@ function scrollPane() {
 
   var y = window.pageYOffset;
 
-  if (yStart < y && y < yEnd) {
+  var newTop;
+  if (y < yStart) {
+    newTop = topStart;
+  } else if (yStart < y && y < yEnd) {
     var delta = topRange * (y - yStart) / (yEnd - yStart);
-    var top = topStart - delta;
-
-    Array.from(document.getElementsByClassName('aside-pane')).forEach(function(item) {
-      item.style.marginTop = top + 'px';
-    })
+    newTop = topStart - delta;
+  } else {
+    newTop = topEnd;
   }
 
+  Array.from(document.getElementsByClassName('aside-pane')).forEach(function(item) {
+    item.style.marginTop = newTop + 'px';
+  })
 }
