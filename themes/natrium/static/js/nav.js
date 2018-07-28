37140cbd6dca6f3a3e2242149cd3d8f2e51820c8
mudox <imudox@gmail.com>
2018-07-28 14:30:42 +0800

    Add aside pane auto scrolling

--- themes/natrium/static/js/nav.js
+++ themes/natrium/static/js/nav.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(window).scroll(function() {
     showAsideNavIfNotTop()
+    scrollPane()
   })
 });
 
@@ -10,4 +11,26 @@ function showAsideNavIfNotTop() {
   } else {
     $('.left-pane-nav').fadeOut()
   }
+}
+
+function scrollPane() {
+  var topStart = 128; // should be the same as in `pane.css`
+  var topEnd = 20;
+  var topRange = topStart - topEnd;
+
+  var yStart = 100;
+  var yEnd = window.innerHeight;
+  var yRange = (yEnd - yStart);
+
+  var y = window.pageYOffset;
+
+  if (yStart < y && y < yEnd) {
+    var delta = topRange * (y - yStart) / (yEnd - yStart);
+    var top = topStart - delta;
+
+    Array.from(document.getElementsByClassName('aside-pane')).forEach(function(item) {
+      item.style.marginTop = top + 'px';
+    })
+  }
+
 }
